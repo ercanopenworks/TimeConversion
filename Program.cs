6e0fea861d06ea9bd67a1c3283aa306680d67cb9
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-05-11 07:26:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static string tConvert(string sTime)
+        {
+            var tType = sTime.Substring(sTime.Length-2).ToUpper();
+            var timeValue = sTime.Substring(0, sTime.Length - 2);
+
+            var seperate = timeValue.Split(':');
+            var hour = Convert.ToInt32(seperate[0]);
+            var min = Convert.ToInt32(seperate[1]);
+            var sec = Convert.ToInt32(seperate[2]);
+            if (tType=="AM" && hour>=12)
+            {
+                hour -= 12;
+
+            }
+            else if (tType == "PM" && hour < 12)
+            {
+                hour += 12;
+
+            }
+            var sHour = (hour < 10) ? 0 + hour.ToString() : hour.ToString();
+            var sMinute = (min < 10) ? 0 + min.ToString() : min.ToString();
+            var sSecond = (sec < 10) ? 0 + sec.ToString() : sec.ToString();
+
+            var result = $"{sHour}:{sMinute}:{sSecond}";
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string sTime = Console.ReadLine();
+            Console.WriteLine(tConvert(sTime));
+        }
+    }
+}
