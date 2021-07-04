f8ec9e3f94817b817889ecd2369ea80043048f7a
Ercan Erdoğan <ercanerdogan@gmail.com>
2021-07-04 18:34:14 +0300

    easiest way was added

--- Program.cs
+++ Program.cs
@@ -7,30 +7,36 @@ namespace TimeConversion
     {
         static string tConvert(string sTime)
         {
-            var tType = sTime.Substring(sTime.Length-2).ToUpper();
-            var timeValue = sTime.Substring(0, sTime.Length - 2);
+            //easiest way
 
-            var seperate = timeValue.Split(':');
-            var hour = Convert.ToInt32(seperate[0]);
-            var min = Convert.ToInt32(seperate[1]);
-            var sec = Convert.ToInt32(seperate[2]);
-            if (tType=="AM" && hour>=12)
-            {
-                hour -= 12;
+            var dt = DateTime.ParseExact(sTime, "hh:mm:sstt", CultureInfo.InvariantCulture);
 
-            }
-            else if (tType == "PM" && hour < 12)
-            {
-                hour += 12;
+            return $"{dt:HH:mm:ss}";
+            //manual parsing
+            // var tType = sTime.Substring(sTime.Length-2).ToUpper();
+            // var timeValue = sTime.Substring(0, sTime.Length - 2);
 
-            }
-            var sHour = (hour < 10) ? 0 + hour.ToString() : hour.ToString();
-            var sMinute = (min < 10) ? 0 + min.ToString() : min.ToString();
-            var sSecond = (sec < 10) ? 0 + sec.ToString() : sec.ToString();
+            // var seperate = timeValue.Split(':');
+            // var hour = Convert.ToInt32(seperate[0]);
+            // var min = Convert.ToInt32(seperate[1]);
+            // var sec = Convert.ToInt32(seperate[2]);
+            // if (tType=="AM" && hour>=12)
+            // {
+            //     hour -= 12;
 
-            var result = $"{sHour}:{sMinute}:{sSecond}";
+            // }
+            // else if (tType == "PM" && hour < 12)
+            // {
+            // hour += 12;
 
-            return result;
+            // }
+            // var sHour = (hour < 10) ? 0 + hour.ToString() : hour.ToString();
+            // var sMinute = (min < 10) ? 0 + min.ToString() : min.ToString();
+            // var sSecond = (sec < 10) ? 0 + sec.ToString() : sec.ToString();
+
+            // var result = $"{sHour}:{sMinute}:{sSecond}";
+
+            // return result;   
         }
         static void Main(string[] args)
         {
